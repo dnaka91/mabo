0f69ed5bf740b6ac113001153bf4632c9a651ee1
Dominik Nakamura <dnaka91@gmail.com>
2023-10-23 19:21:02 +0900

    refactor: transform some tests into snapshot tests

--- crates/stef-build/tests/compiler.rs
+++ crates/stef-build/tests/compiler.rs
@@ -14,3 +14,15 @@ fn compile_schema() {
         assert_snapshot!("compile", format!("{value}"), input.trim());
     });
 }
+
+#[test]
+fn compile_schema_extra() {
+    glob!("inputs-extra/*.stef", |path| {
+        let input = fs::read_to_string(path).unwrap();
+        let value = Schema::parse(input.as_str()).unwrap();
+        let value = stef_build::compile_schema(&value);
+        let value = prettyplease::unparse(&syn::parse2(value.clone()).unwrap());
+
+        assert_snapshot!("compile-extra", format!("{value}"), input.trim());
+    });
+}

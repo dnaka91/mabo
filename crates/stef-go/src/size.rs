6f9ae1326c8b96ecf737db9f7c9e222ef4d25695
Dominik Nakamura <dnaka91@gmail.com>
2023-12-12 14:38:20 +0900

    WIP

--- crates/stef-go/src/size.rs
+++ crates/stef-go/src/size.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::too_many_lines)]
 
-use std::fmt::{self, Display};
+use std::fmt::{self, write, Display, Write};
 
 use stef_parser::{DataType, Fields, Generics, Struct, Type, Variant};
 
@@ -8,29 +8,248 @@ use crate::definition::{self, RenderGenericNames};
 
 pub(super) struct RenderStruct<'a>(pub(super) &'a Struct<'a>);
 
+struct GoFunc<'a, T>(&'a mut T);
+
+impl<T: Write> GoFunc<'_, T> {
+    fn receiver(self, name: impl Display, ty: impl Display) -> Result<Self, fmt::Error> {
+        write!(self.0, " ({name} {ty})")?;
+        Ok(self)
+    }
+
+    fn named(self, name: impl Display) -> Result<Self, fmt::Error> {
+        write!(self.0, " {name}")?;
+        Ok(self)
+    }
+
+    fn params(self, params: impl IntoIterator<Item = impl Display>) -> Result<Self, fmt::Error> {
+        self.0.write_char('(')?;
+        for (i, param) in params.into_iter().enumerate() {
+            if i > 0 {
+                self.0.write_str(", ")?;
+            }
+            write!(self.0, "{param}")?;
+        }
+        self.0.write_char(')')?;
+
+        Ok(self)
+    }
+
+    fn no_params(self) -> Result<Self, fmt::Error> {
+        self.0.write_str("()")?;
+        Ok(self)
+    }
+
+    fn returns(
+        self,
+        returns: impl IntoIterator<
+            Item = impl Display,
+            IntoIter = impl ExactSizeIterator<Item = impl Display>,
+        >,
+    ) -> Result<Self, fmt::Error> {
+        self.0.write_char(' ')?;
+
+        let returns = returns.into_iter();
+        let multi = returns.len() > 1;
+
+        if multi {
+            self.0.write_char('(')?;
+        }
+        for (i, ret) in returns.enumerate() {
+            if i > 0 {
+                self.0.write_str(", ")?;
+            }
+            write!(self.0, "{ret}")?;
+        }
+        if multi {
+            self.0.write_char(')')?;
+        }
+
+        Ok(self)
+    }
+
+    fn body(self, build: impl FnOnce(usize, &mut T) -> fmt::Result) -> fmt::Result {
+        self.0.write_str(" {\n")?;
+        build(1, self.0)?;
+        self.0.write_str("}")
+    }
+}
+
+fn func<T: Write>(w: &mut T) -> Result<GoFunc<'_, T>, fmt::Error> {
+    w.write_str("func")?;
+    Ok(GoFunc(w))
+}
+
+struct GoCall<'a, T> {
+    w: &'a mut T,
+    multiline: bool,
+    params_open: bool,
+    params_count: usize,
+}
+
+impl<T: Write> GoCall<'_, T> {
+    fn generics(self, types: impl Display) -> Result<Self, fmt::Error> {
+        write!(self.w, "[{types}]")?;
+        Ok(self)
+    }
+
+    fn multiline(mut self) -> Self {
+        self.multiline = true;
+        self
+    }
+
+    fn param(mut self, param: impl Display) -> Result<Self, fmt::Error> {
+        if !self.params_open {
+            self.w.write_char('(')?;
+            self.params_open = true;
+        }
+
+        if self.params_count > 0 {
+            self.w.write_str(", ")?;
+        }
+
+        write!(self.w, "{param}")?;
+        self.params_count += 1;
+
+        Ok(self)
+    }
+
+    fn param_with(mut self, build: impl FnOnce(&mut T) -> fmt::Result) -> Result<Self, fmt::Error> {
+        if !self.params_open {
+            self.w.write_char('(')?;
+            self.params_open = true;
+        }
+
+        if self.params_count > 0 {
+            self.w.write_str(", ")?;
+        }
+
+        build(self.w)?;
+        self.params_count += 1;
+
+        Ok(self)
+    }
+
+    fn end(self) -> fmt::Result {
+        // if self.multiline {
+        //     write_indent(self.w, self.indent)?;
+        // }
+        self.w.write_str(")\n")
+    }
+}
+
+fn call<T: Write>(
+    w: &mut T,
+    indent: usize,
+    name: impl Display,
+) -> Result<GoCall<'_, T>, fmt::Error> {
+    write_indent(w, indent)?;
+    write!(w, "{name}")?;
+    Ok(GoCall {
+        w,
+        multiline: false,
+        params_open: false,
+        params_count: 0,
+    })
+}
+
+fn ensure_impl(w: &mut impl Write, interface: impl Display, ty: impl Display) -> fmt::Result {
+    writeln!(w, "var _ {interface} = (*{ty})(nil)\n",)
+}
+
+fn write_indent(w: &mut impl Write, indent: usize) -> fmt::Result {
+    write!(w, "{:\t<indent$}", "",)
+}
+
+fn assign(
+    w: &mut impl Write,
+    indent: usize,
+    variable: impl Display,
+    op: Op,
+    expr: impl Display,
+) -> fmt::Result {
+    write_indent(w, indent)?;
+    writeln!(w, "{variable} {op} {expr}")
+}
+
+fn assign_with<T: Write>(
+    w: &mut T,
+    indent: usize,
+    variable: impl Display,
+    op: Op,
+    expr: impl FnOnce(usize, &mut T) -> fmt::Result,
+) -> fmt::Result {
+    write_indent(w, indent)?;
+    write!(w, "{variable} {op} ")?;
+    expr(indent, w)
+}
+
+enum Op {
+    Init,
+    Assign,
+    AddAssign,
+}
+
+impl Display for Op {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            Self::Init => ":=",
+            Self::Assign => "=",
+            Self::AddAssign => "+=",
+        })
+    }
+}
+
+fn retval(
+    w: &mut impl Write,
+    indent: usize,
+    values: impl IntoIterator<Item = impl Display>,
+) -> fmt::Result {
+    write_indent(w, indent)?;
+    w.write_str("return ")?;
+    for (i, value) in values.into_iter().enumerate() {
+        if i > 0 {
+            w.write_str(", ")?;
+        }
+        write!(w, "{value}")?;
+    }
+    w.write_char('\n')
+}
+
 impl Display for RenderStruct<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
+        ensure_impl(
             f,
-            "var _ buf.Size = (*{}{})(nil)\n",
-            heck::AsUpperCamelCase(&self.0.name),
-            RenderGenericNames {
-                generics: &self.0.generics,
-                fields_filter: None,
-            }
+            "buf.Size",
+            format_args!(
+                "{}{}",
+                heck::AsUpperCamelCase(&self.0.name),
+                RenderGenericNames {
+                    generics: &self.0.generics,
+                    fields_filter: None,
+                }
+            ),
         )?;
 
-        writeln!(
-            f,
-            "func (v *{}{}) Size() int {{",
-            heck::AsUpperCamelCase(&self.0.name),
-            RenderGenericNames {
-                generics: &self.0.generics,
-                fields_filter: None,
-            }
-        )?;
-        writeln!(f, "\tsize := 0")?;
-        writeln!(f, "{}\treturn size\n}}", RenderFields(&self.0.fields))
+        func(f)?
+            .receiver(
+                "v",
+                format_args!(
+                    "*{}{}",
+                    heck::AsUpperCamelCase(&self.0.name),
+                    RenderGenericNames {
+                        generics: &self.0.generics,
+                        fields_filter: None,
+                    }
+                ),
+            )?
+            .named("Size")?
+            .no_params()?
+            .returns(["int"])?
+            .body(|indent, f| {
+                assign(f, indent, "size", Op::Init, 0)?;
+                write!(f, "{}", RenderFields(&self.0.fields))?;
+                retval(f, indent, ["size"])
+            })
     }
 }
 
@@ -42,29 +261,41 @@ pub(super) struct RenderEnumVariant<'a> {
 
 impl Display for RenderEnumVariant<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
+        ensure_impl(
             f,
-            "var _ buf.Size = (*{}_{}{})(nil)\n",
-            heck::AsUpperCamelCase(self.enum_name),
-            heck::AsUpperCamelCase(&self.variant.name),
-            RenderGenericNames {
-                generics: self.generics,
-                fields_filter: Some(&self.variant.fields),
-            }
+            "buf.Size",
+            format_args!(
+                "{}_{}{}",
+                heck::AsUpperCamelCase(self.enum_name),
+                heck::AsUpperCamelCase(&self.variant.name),
+                RenderGenericNames {
+                    generics: self.generics,
+                    fields_filter: Some(&self.variant.fields),
+                }
+            ),
         )?;
 
-        writeln!(
-            f,
-            "func (v *{}_{}{}) Size() int {{",
-            heck::AsUpperCamelCase(self.enum_name),
-            heck::AsUpperCamelCase(&self.variant.name),
-            RenderGenericNames {
-                generics: self.generics,
-                fields_filter: Some(&self.variant.fields),
-            }
-        )?;
-        writeln!(f, "\tsize := 0")?;
-        writeln!(f, "{}\treturn size\n}}", RenderFields(&self.variant.fields))
+        func(f)?
+            .receiver(
+                "v",
+                format_args!(
+                    "*{}_{}{}",
+                    heck::AsUpperCamelCase(self.enum_name),
+                    heck::AsUpperCamelCase(&self.variant.name),
+                    RenderGenericNames {
+                        generics: self.generics,
+                        fields_filter: Some(&self.variant.fields),
+                    }
+                ),
+            )?
+            .named("Size")?
+            .no_params()?
+            .returns(["int"])?
+            .body(|indent, f| {
+                assign(f, indent, "size", Op::Init, 0)?;
+                write!(f, "{}", RenderFields(&self.variant.fields))?;
+                retval(f, indent, ["size"])
+            })
     }
 }
 
@@ -76,30 +307,51 @@ impl Display for RenderFields<'_> {
             Fields::Named(named) => {
                 for field in named {
                     if let DataType::Option(ty) = &field.ty.value {
-                        writeln!(
-                            f,
-                            "\tsize += buf.SizeFieldOption[{}]({}, &v.{}, func (v {0}) []byte \
-                             {{\n\t\treturn {}\n\t}})",
-                            definition::RenderType(ty),
-                            field.id.get(),
-                            heck::AsUpperCamelCase(&field.name),
-                            RenderType {
-                                ty,
-                                name: "v",
-                                indent: 2,
-                            },
-                        )?;
+                        assign_with(f, 1, "size", Op::AddAssign, |indent, f| {
+                            call(f, 0, "buf.SizeFieldOption")?
+                                .generics(definition::RenderType(ty))?
+                                .param(field.id.get())?
+                                .param(format_args!("&v.{}", heck::AsUpperCamelCase(&field.name)))?
+                                .param_with(|f| {
+                                    func(f)?
+                                        .params([format_args!("v {}", definition::RenderType(ty))])?
+                                        .returns(["[]byte"])?
+                                        .body(|_indent, f| {
+                                            retval(
+                                                f,
+                                                indent + 1,
+                                                [RenderType {
+                                                    ty,
+                                                    name: "v",
+                                                    indent: 2,
+                                                }],
+                                            )
+                                        })
+                                })?
+                                .end()
+                        })?;
                     } else {
-                        writeln!(
-                            f,
-                            "\tsize += buf.SizeField({}, func() int {{\n\t\treturn {}\n\t}})",
-                            field.id.get(),
-                            RenderType {
-                                ty: &field.ty,
-                                name: format_args!("v.{}", heck::AsUpperCamelCase(&field.name)),
-                                indent: 2,
-                            },
-                        )?;
+                        assign_with(f, 1, "size", Op::AddAssign, |_, f| {
+                            call(f, 0, "buf.SizeField")?
+                                .param(field.id.get())?
+                                .param_with(|f| {
+                                    func(f)?.no_params()?.returns(["int"])?.body(|indent, f| {
+                                        retval(
+                                            f,
+                                            indent + 1,
+                                            [RenderType {
+                                                ty: &field.ty,
+                                                name: format_args!(
+                                                    "v.{}",
+                                                    heck::AsUpperCamelCase(&field.name)
+                                                ),
+                                                indent: 2,
+                                            }],
+                                        )
+                                    })
+                                })?
+                                .end()
+                        })?;
                     }
                 }
 
@@ -134,7 +386,9 @@ impl Display for RenderFields<'_> {
                     }
                 }
 
-                writeln!(f, "\tsize += buf.SizeU32(buf.EndMarker)")?;
+                assign_with(f, 1, "size", Op::AddAssign, |_, f| {
+                    call(f, 0, "buf.SizeU32")?.param("buf.EndMarker")?.end()
+                })?;
             }
             Fields::Unit => {}
         }
@@ -155,7 +409,7 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self.ty.value {
-            DataType::Bool => write!(f, "buf.SizeBool({})", self.name),
+            DataType::Bool => call(f, 0, "buf.SizeBool")?.param(&self.name)?.end(),
             DataType::U8 => write!(f, "buf.SizeU8({})", self.name),
             DataType::U16 => write!(f, "buf.SizeU16({})", self.name),
             DataType::U32 => write!(f, "buf.SizeU32({})", self.name),
@@ -174,26 +428,26 @@ where
             DataType::Bytes | DataType::BytesRef | DataType::BoxBytes => {
                 write!(f, "buf.SizeBytes({})", self.name)
             }
-            DataType::Vec(ty) => {
-                writeln!(
-                    f,
-                    "buf.SizeVec[{}]({}, func(v {0}) int {{",
-                    definition::RenderType(ty),
-                    self.name
-                )?;
-                writeln!(
-                    f,
-                    "{:\t<indent$}return {}",
-                    "",
-                    RenderType {
-                        ty,
-                        name: "v",
-                        indent: self.indent + 1
-                    },
-                    indent = self.indent + 1,
-                )?;
-                write!(f, "{:\t<indent$}}})", "", indent = self.indent)
-            }
+            DataType::Vec(ty) => call(f, 0, "buf.SizeVec")?
+                .generics(definition::RenderType(ty))?
+                .param(&self.name)?
+                .param_with(|f| {
+                    func(f)?
+                        .params([format_args!("v {}", definition::RenderType(ty))])?
+                        .returns(["int"])?
+                        .body(|indent, f| {
+                            retval(
+                                f,
+                                indent+1,
+                                [RenderType {
+                                    ty,
+                                    name: "v",
+                                    indent: indent + 1,
+                                }],
+                            )
+                        })
+                })?
+                .end(),
             DataType::HashMap(kv) => {
                 writeln!(
                     f,
